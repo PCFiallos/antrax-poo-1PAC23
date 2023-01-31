8d04978cd7dceea8b956f7384f164c2d6b1c71c1
PCFiallos <cesarfiallos2000@gmail.com>
2023-01-30 18:55:35 -0600

    init

---
+++ unidad1/basico/basico/Program.cs
@@ -0,0 +1,18 @@
+﻿using basico.tipo_datos;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace basico
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("hola desde program");
+            Tipos01 t01 = new Tipos01();
+        }
+    }
+}
---
+++ unidad1/basico/basico/tipo-datos/Tipos01.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace basico.tipo_datos
+{
+
+    internal class Tipos01
+
+    {
+        public String saludo { get; set; }
+        public Tipos01(String v1)
+        {
+            
+            this.saludo = v1;
+            String sNombre = this.saludo + "Juan Perez";
+            Console
+                .WriteLine(sNombre);
+
+            
+           
+        }
+    }
+}
